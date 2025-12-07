7717036ef7c7fc0ec46ea8365efee0d0c669fca3
brqnko <tenamen.12@gmail.com>
2025-12-07 14:25:46 +0900

    revert day05

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -1,5 +1,33 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("{:?}", (0..1145141919).max());
+    // part 1
+    let mut buffer = String::new();
+    let mut ranges = Vec::new();
+    loop {
+        std::io::stdin().read_line(&mut buffer)?;
+        if buffer.len() < 3 {
+            break;
+        }
+        buffer = buffer.trim().to_string();
+        let bar_idx = buffer.find("-").unwrap();
+        ranges.push(buffer[0..bar_idx].parse::<u128>()?..=buffer[bar_idx + 1..].parse::<u128>()?);
+        buffer.clear();
+    }
+
+    ranges.sort_by_key(|r| *r.start());
+
+    let mut ans = 0;
+    let mut rust = None;
+    for range in ranges {
+        if range.end() <= &rust.unwrap_or_default() {
+            continue;
+        }
+        ans += range.end() - range.start().max(&rust.unwrap_or_default());
+        if &rust.unwrap_or_default() < range.start() {
+            ans += 1;
+        }
+        rust = Some(*range.end());
+    }
+    println!("{ans}");
     Ok(())
 }
 
